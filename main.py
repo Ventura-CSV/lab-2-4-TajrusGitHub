653bd32de947b1198ab10a1834f4bb9962085c86
Tajrus Lipscomb Jr. <tajrus_lipscomb1@my.vcccd.edu>
2023-10-01 22:02:45 -0700

    String

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
-def main():
-    ##################################################
-    # Comlete your code here
-    ##################################################
-    pass
+def string_operations():
+    original_str = "Python Programming"
 
+    # Extract "Python" using index slicing
+    sub1 = original_str[:6]
+
+    # Extract "Programming" using index slicing
+    sub2 = original_str[7:]
+
+    # Merge the substrings using concatenation
+    merged_str = sub2 + " " + sub1
+
+    print("Original String:", original_str)
+    print("Extracted Substring 1:", sub1)
+    print("Extracted Substring 2:", sub2)
+    print("Merged String:", merged_str)
 
 if __name__ == '__main__':
-    main()
+    string_operations()
