a12257958c376e9a10b49e5175ad1091784b34c3
Tajrus Lipscomb Jr. <tajrus_lipscomb1@my.vcccd.edu>
2023-10-01 22:35:38 -0700

    String

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-import io
-import sys
-import re
-
 def main():
     original_str = "Python Programming"
 
@@ -21,25 +17,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
-def test_main_1():
-    captureOut = io.StringIO()
-    sys.stdout = captureOut
-
-    main()
-
-    sys.stdout = sys.__stdout__
-
-    lines = captureOut.getvalue().split('\n')
-
-    res = re.search(r'[\w,\W]*Programming[\w,\W]*', lines[2])
-    assert res is not None
-
-    res = re.search(r'[\w,\W]*Python[\w,\W]*', lines[1])
-    assert res is not None
-
-    res = re.search(r'[\w,\W]*Programming[\w,\W]*Python[\w,\W]*', lines[3])
-    assert res is not None
-
-if __name__ == '__main__':
-    test_main_1()
--- main_test.py
+++ main_test.py
@@ -1,29 +1,27 @@
-import main
 import io
 import sys
 import re
-
+import main
 
 def test_main_1():
     captureOut = io.StringIO()
     sys.stdout = captureOut
-    # datastr = '10 \n 20 \n 30'
-    # sys.stdin = io.StringIO(datastr)
 
     main.main()
-    sys.stdout = sys.__stdout__
-    print('Captured ', captureOut.getvalue())
-    lines = captureOut.getvalue().split('\n')
-    print(lines)
 
-    res = re.search(r'[\w,\W]*Programming[\w,\W]*', lines[0])
-    assert res != None
-    print(res.group())
+    sys.stdout = sys.__stdout__
+
+    lines = captureOut.getvalue().split('\n')
+
+    res = re.search(r'[\w,\W]*Programming[\w,\W]*', lines[2])
+    assert res is not None
 
     res = re.search(r'[\w,\W]*Python[\w,\W]*', lines[1])
-    assert res != None
-    print(res.group())
+    assert res is not None
 
-    res = re.search(r'[\w,\W]*Programming[\w,\W]*Python[\w,\W]*', lines[2])
-    assert res != None
-    print(res.group())
+    res = re.search(r'[\w,\W]*Programming[\w,\W]*Python[\w,\W]*', lines[3])
+    assert res is not None
+
+if __name__ == '__main__':
+    test_main_1()
+
